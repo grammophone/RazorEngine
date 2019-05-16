5c142843db49317f0825663ac2992b26bd906cff
Bruce Liu <liuzuliang0813@163.com>
2019-05-16 14:04:49 +0800

    New Console Aplication - RazorEngine.NetCore.ConsoleTest

---
+++ RazorEngine.NetCore.ConsoleTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RazorEngine;
+using RazorEngine.Templating;
+
+namespace RazorEngine.NetCore.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string template = "Hello @Model.Name, welcome to RazorEngine!";
+            //string template = "@inherits RazorEngine.Templating.TemplateBase<Dynamic>\r\n Hello @Model.Name, welcome to RazorEngine! ";
+
+            var result =
+                Engine.Razor.RunCompile(template, "templateKey", null, new { Name = "World" });
+
+            Console.WriteLine("Template: {0}", template);
+            Console.WriteLine("Result: {0}", result);
+        }
+    }
+}
